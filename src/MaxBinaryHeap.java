746d2c663d08566295e2f12f4e79da884d1bdae7
Ahmed Khaled <ahmedkhaled1123300@gmail.com>
2017-03-09 11:25:04 +0200

    implemented max-heapify method

--- src/MaxBinaryHeap.java
+++ src/MaxBinaryHeap.java
@@ -5,7 +5,8 @@ import java.util.function.Predicate;
  * Created by Ahmed Khaled on 09/03/2017.
  */
 public class MaxBinaryHeap {
-    private ArrayList<?> heap;
+
+    private List<? extends Comparable> heap;
     private int size;
 
     public MaxBinaryHeap() {
@@ -13,26 +14,45 @@ public class MaxBinaryHeap {
         size = 0;
     }
 
-    public MaxBinaryHeap(Object[] array) {
+    public MaxBinaryHeap(Comparable[] array) {
         heap = new ArrayList<>(Arrays.asList(array));
         size = array.length;
     }
 
-    public MaxBinaryHeap(List<? extends Object> array) {
+    public MaxBinaryHeap(Collection<? extends Comparable> array) {
         heap = new ArrayList<>(array);
-        size = array.size();
-    }
-
-    public MaxBinaryHeap(Collection<? extends Object> array) {
-        heap = new ArrayList<>(Arrays.asList(array));
     }
 
     public void maxHeapInsert(Object element) {
         size++;
     }
 
-    private void maxHeapify() {
+    public Object[] getAsArray() {
+        return heap.toArray();
+    }
 
+    public Collection<?> getAsCollection() {
+        return heap;
+    }
+
+    private void maxHeapify(int i) {
+        int left = left(i);
+        int right = right(i);
+        int largest = -1;
+        if (left < size && heap.get(left).compareTo(heap.get(i)) > 0 ) {
+            largest = left;
+        } else {
+            largest = i;
+        }
+        if (right < size && heap.get(right).compareTo(heap.get(largest)) > 0 ) {
+            largest = right;
+        } else {
+            largest = i;
+        }
+        if (largest != i) {
+            Collections.swap(heap, i, largest);
+            maxHeapify(largest);
+        }
     }
 
     private void buildMaxHeap() {
@@ -58,19 +78,19 @@ public class MaxBinaryHeap {
         return i / 2;
     }
 
-    private Object leftChild(int i) {
-        return heap.get(i * 2);
+    private int left(int i) {
+        return i * 2;
     }
 
-    private Object rightChild(int i) {
-        return heap.get(i * 2 + 1);
+    private int right(int i) {
+        return i * 2 + 1;
     }
 
-    private boolean hasLeftChild(int i) {
+    private boolean hasLeft(int i) {
         return i * 2 < size;
     }
 
-    private boolean hasRightChild(int i) {
+    private boolean hasRight(int i) {
         return i * 2 + 1 < size;
     }
 }
