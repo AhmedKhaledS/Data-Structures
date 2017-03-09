a283c4e0c761aa661962020911ea6ab3439534f7
Ahmed Khaled <ahmedkhaled1123300@gmail.com>
2017-03-09 17:21:35 +0200

    Done max Heap with sorting

--- src/Test.java
+++ src/Test.java
@@ -8,6 +8,11 @@ public class Test {
     public static void main(String argv[]) {
         String[] arr = {"c", "b", "aaa", "a"};
         MaxHeap<String> mx = new MaxHeap<>(arr);
+        mx.insert("aa");
+        mx.insert("bb");
+        mx.insert("z");
+        mx.insert("sa");
+        mx.insert("aaaabbababasasva");
         mx.heapSort();
         Collection<String> res = mx.getAsCollection();
         for (String x : res) {
