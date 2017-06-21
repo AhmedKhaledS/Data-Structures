f66cc83bf78e8e6eff33a4d7c34ba771618c8f2d
Ahmed Khaled <ahmedkhaled1123300@gmail.com>
2017-06-21 10:20:56 +0200

    added implementation for AdjacentNodeList

--- src/sssp/AdjacentNodeList.java
+++ src/sssp/AdjacentNodeList.java
@@ -1,10 +1,39 @@
 package sssp;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by Ahmed Khaled on 21/06/2017.
  */
 public class AdjacentNodeList {
-	List<>
+
+	private List<Pair<Integer, Double>> adjNodes;
+	private List<Integer> adjacentNodes;
+
+	/**
+	 * Constructor to initialize the set of neighbor nodes with the passed data.
+	 * @param nodes the array-list that holds the neighbour nodes with data.
+	 */
+	public AdjacentNodeList(ArrayList<Pair<Integer, Double>> nodes) {
+		adjNodes = nodes;
+		adjacentNodes = new ArrayList<Integer>();
+		for (Pair pr : nodes) {
+			adjacentNodes.add((Integer) pr.att1);
+		}
+	}
+
+	/**
+	 * Returns the neighbour nodes of certatin node.
+	 * @return Array-list containing all neighbours.
+	 */
+	public ArrayList<Integer> getAdjNodes() {
+		return (ArrayList<Integer>) adjacentNodes;
+	}
+
+	public void addNode(int node, double weight) {
+		adjNodes.add(new Pair<>(node, weight));
+		adjacentNodes.add(node);
+	}
+
 }
